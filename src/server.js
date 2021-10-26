3de09af0e5251c5836097a531ffa69a2338a0d28
nyhwbh <87648321+nyhwbh@users.noreply.github.com>
2021-10-26 15:07:12 +0900

    get request

--- src/server.js
+++ src/server.js
@@ -4,7 +4,12 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = () => 
+ console.log("Somebody is trying to go home");
+
+app.get("/", handleHome)
+
 const handleListening = () =>
  console.log(`Server listening on port http://localhost:${PORT}`);
 
-app.listen(PORT, handleListening)
+app.listen(PORT, handleListening);
