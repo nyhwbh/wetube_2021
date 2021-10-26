1a5111519f9f1406b2678f68df153f6728b8b1bc
nyhwbh <87648321+nyhwbh@users.noreply.github.com>
2021-10-26 21:10:58 +0900

    install_morgan

--- src/server.js
+++ src/server.js
@@ -1,19 +1,23 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const handleHome = (req, res) => {
-    return res.send("<h1>Hello there.</h1>");
-}
-const handleLogin = (req, res) => {
-    return res.send("Login here.");
-}
+const home = (req, res) => {
+    return res.send("hello");
+};
 
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+const login = (req, res) => {
+    return res.send("login");
+  };
 
+  app.use(logger);
+  app.get("/", home);
+  app.get("/login", login);
+  
 const handleListening = () =>
  console.log(`Server listening on port http://localhost:${PORT}`);
 
