7cf8effaaaab55d5e6ef8c337448487ec02841bf
nyhwbh <87648321+nyhwbh@users.noreply.github.com>
2021-10-27 16:00:46 +0900

    router

--- src/server.js
+++ src/server.js
@@ -5,19 +5,30 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
+app.use(logger);
 
-const home = (req, res) => {
-    return res.send("hello");
-};
+const globalRouter = express.Router();
 
-const login = (req, res) => {
-    return res.send("login");
-  };
+const handlehome = (req, res) =>res.send("Home");
 
-  app.use(logger);
-  app.get("/", home);
-  app.get("/login", login);
-  
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/users",userRouter);
+app.use("/videos", videoRouter);
+
 const handleListening = () =>
  console.log(`Server listening on port http://localhost:${PORT}`);
 
