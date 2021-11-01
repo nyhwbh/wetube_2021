e74aca98190c7d009e81d695cef3e6f602643d2c
nyhwbh <87648321+nyhwbh@users.noreply.github.com>
2021-11-01 17:40:08 +0900

    videodetail

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -15,9 +15,11 @@ export const home = async (req, res) => {
   const videos = await Video.find({});
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const watch = (req, res) => {
+
+export const watch = async (req, res) => {
   const { id } = req.params;
-  return res.render("watch", { pageTitle: `Watching`});
+  const video = await Video.findById(id);
+  return res.render("watch", { pageTitle: video.title}, video);
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
@@ -30,21 +32,24 @@ export const postEdit = (req, res) => {
   return res.redirect(`/videos/${id}`);
 };
 
-  export const getUpload = (req, res) => {
-    return res.render("upload", { pageTitle: "Upload Video" });
-  };
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
   
-  export const postUpload = async (req, res) => {
-    const { title, description, hashtags } = req.body;
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  try{
     await Video.create({
       title,
       description,
-      createdAt: Date.now(),
       hashtags: hashtags.split(",").map((word) => `#${word}`),
-      meta: {
-        views: 0,
-        rating: 0,
-      },
     });
     return res.redirect("/");
-  };
+  } 
+  catch (error) {
+  return res.render("upload", {
+    pageTitle: "Upload Video",
+    errorMessage: error._message,
+    });
+  }
+};
