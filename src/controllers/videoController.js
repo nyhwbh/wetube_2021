6f8d745f2ae8f1d4b459b8585c17459f0b5a8203
nyhwbh <87648321+nyhwbh@users.noreply.github.com>
2021-11-01 20:44:11 +0900

    middlewares

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,16 +1,5 @@
 import Video from "../models/Video";
 
-/*
-export const home = (req, res) => {
-  console.log("Start");
-  Video.find({}, (error, videos) => {
-    if(error){
-    return res.render("server-error")
-    }
-    return res.render("home", {pageTitle:"Home", videos});
-  });
-  console.log("I finish first");
-*/
 export const home = async (req, res) => {
   const videos = await Video.find({});
   return res.render("home", { pageTitle: "Home", videos });
@@ -44,9 +33,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: hashtags
-      .split(",")
-      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -61,9 +48,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags
-        .split(",")
-        .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+      hashtags : Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
   } 
--- src/db.js
+++ src/db.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
 mongoose.connect("mongodb://127.0.0.1:27017/wetube",{ 
-    useNewUrlParser: true,
+   /* useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,*/
 });
 
 const db = mongoose.connection;
